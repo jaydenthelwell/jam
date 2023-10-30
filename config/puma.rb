3f49530eae538f3a8e50a67b62fe2d1c0701721d
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-10-30 12:04:46 +0000

    error

--- config/puma.rb
+++ config/puma.rb
@@ -41,22 +41,3 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
-
-preload_app!
-
-rackup      DefaultRackup if defined?(DefaultRackup)
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker-specific setup for Rails 4.1 to 5.2, after 5.2 it's not needed
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
-    ActiveRecord::Base.establish_connection(config)
-end
