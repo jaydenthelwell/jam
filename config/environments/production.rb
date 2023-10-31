de43fe3194b4a3f4f10d41e121d77ba9345c74f2
jaydenthelwell <jaydenthelwell02@gmail.com>
2023-10-31 10:25:53 +0000

    True

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   #^^^^^^^^^^^^^^ ADDED BY JURGEST, MIGHT CAUSE ERROOOOORSSSSSSSSSSS ************************************** ^^^^^^^^^^
