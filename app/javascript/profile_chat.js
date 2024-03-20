732c119732f760fdcf26e9b852e908863594141d
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-20 10:42:42 +0000

    Deleted profile chat

--- app/javascript/profile_chat.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const chat = document.getElementById("chat");
-
-  chat.addEventListener("click", () => {
-    chat.classList.remove("d-none");
-  });
-});
