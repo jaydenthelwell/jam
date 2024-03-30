dc60ca1112420a69e9b333f7fd2a7db040427b16
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-03-30 17:09:31 +0100

    Certificates

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
 # config/initializers/redis.rb
 require 'redis'
 
+# Disable SSL certificate verification (not recommended for production)
 Redis.current = Redis.new(url: ENV['REDIS_TLS_URL'], ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
