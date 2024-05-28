856739ee985a0b286d87597c9293ce480af321c8
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-05-28 18:02:09 +0200

    user devise controller added

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,7 @@
 require 'redis'
 
 # Disable SSL certificate verification (not recommended for production)
-Redis.current = Redis.new(url: ENV['REDIS_TLS_URL'], ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+$redis = Redis.new(url: ENV['REDIS_TLS_URL'], ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
+# Or if you're using a Redis namespace:
+# $redis = Redis::Namespace.new(redis_config['namespace'], redis: Redis.new(url: redis_config['url']))
