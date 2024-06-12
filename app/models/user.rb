23c80bc218f050fee581f9a0be0546b70dd92870
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-06-12 23:37:33 +0100

    user params to sign up params

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   attr_accessor :go_to_karaoke
   attr_accessor :description
   attr_accessor :photos
-
+  attr_accessor :name
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
