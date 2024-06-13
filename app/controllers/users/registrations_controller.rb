2f09a342dd2d238c9db52c3ce015b248e45e7c57
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-06-13 10:31:44 +0100

    deleted code in users controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,19 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  def create
-    @user = User.new(sign_up_params)
-    if @user.save
-      # Handle successful user creation (e.g., redirect to a confirmation page)
-    else
-      # Handle failed user creation (e.g., render the registration form with errors)
-      render :new
-    end
-  end
-
-  def new
-    @user = User.new
-  end
 
   private
+
+  # def after_sign_up_path_for(resource_or_scope)
+  #   Profile.create(user: current_user)
+  #   '/top_genres/new'
+  # end
 
   def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name, :d_o_b, :location, :gender, :on_repeat, :all_time_favorite, :go_to_karaoke, :description, photos: [])
