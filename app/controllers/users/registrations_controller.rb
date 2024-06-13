0aa9750e38018f8ea79a961696e5263b50067830
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-06-13 10:47:44 +0100

    dob -> date_of_birth

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
-  # def after_sign_up_path_for(resource_or_scope)
-  #   Profile.create(user: current_user)
-  #   '/top_genres/new'
-  # end
+  def after_sign_up_path_for(resource_or_scope)
+    Profile.create(user: current_user)
+    '/top_genres/new'
+  end
 
   def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name, :date_of_birth, :location, :gender, :on_repeat, :all_time_favorite, :go_to_karaoke, :description, photos: [])
