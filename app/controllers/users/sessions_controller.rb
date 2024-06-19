ae1c887d000aa8ceae9621553effe3c6faecf69a
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-06-19 13:50:34 +0100

    Sessions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,13 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def create
+    super do |resource|
+      if resource.persisted?
+        flash[:notice] = "Signed in successfully."
+        Rails.logger.info("User #{resource.email} logged in successfully.")
+      else
+        flash[:alert] = "Invalid email or password."
+        Rails.logger.warn("Login attempt failed for email #{params[:user][:email]}")
+      end
+    end
+  end
 end
